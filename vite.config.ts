978575013bacf6898605cbe21b2b26c75873db56
Qassim <127034238+Qassim-Coding@users.noreply.github.com>
2025-11-21 13:39:55 +0100

    changement v Final
    Changement files avant mise en prod

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Cast process to any to avoid TS error because Node types might not be fully loaded
-  const env = loadEnv(mode, (process as any).cwd(), '');
+  // On charge les variables d'environnement
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
     define: {
-      // This allows your existing code using process.env.API_KEY to work
-      // In production (Vercel), this pulls from the Environment Variables dashboard.
+      // Mappe la variable d'env pour qu'elle soit accessible via process.env.API_KEY dans le code
       'process.env.API_KEY': JSON.stringify(env.API_KEY || env.VITE_API_KEY)
     }
   };
